851130a2a8c700f0e8662cd69798bd84a8828618
spicydog <spicydog@gmail.com>
2016-11-12 22:18:04 +0700

    Create Topic and Comment models
    1. Run "php artisan make:model Topic" and "php artisan make:model Comment" to create models.
    2. Add database relationships to the model files as in the commit.

---
+++ app/Comment.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    /**
+     * Get a topic of a given comment
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function topic()
+    {
+        return $this->belongsTo('App\Topic');
+    }
+
+    /**
+     * Get a user of a given comment
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+}
---
+++ app/Topic.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Topic extends Model
+{
+    /**
+     * Get the Comments of a given topic.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function comments()
+    {
+        return $this->hasMany('App\Comment');
+    }
+
+    /**
+     * Get a user of a given topic
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+}
